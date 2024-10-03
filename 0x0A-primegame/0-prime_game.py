159a3d4a8b9a0b6f969d6b466b65df76bd3973e7
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-10-03 04:31:32 -0700

    {update 0 file}

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,55 @@
 #!/usr/bin/python3
-"""0. Prime Game script"""
+"""
+Prime Game Module.
+"""
 
 
 def isWinner(x, nums):
-    if x <= 0 or nums is None:
+    """
+    Determines the winner of the Prime Game over multiple rounds.
+    Returns:
+        str or None: The name of the player who won the most rounds ("Maria" or "Ben"),
+                     or None if there is no clear winner.
+    """
+    if not isinstance(x, int) or x < 1:
         return None
-    if x != len(nums):
+    if not isinstance(nums, list) or len(nums) != x:
         return None
 
-    ben = 0
-    maria = 0
+    max_n = max(nums) if nums else 0
+    sieve = [True] * (max_n + 1)
+    sieve[0:2] = [False, False]
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
-    a[0], a[1] = 0, 0
-    for i in range(2, len(a)):
-        rm_multiples(a, i)
+    # Sieve of Eratosthenes to identify primes up to max_n
+    for current in range(2, int(max_n**0.5) + 1):
+        if sieve[current]:
+            for multiple in range(current*current, max_n + 1, current):
+                sieve[multiple] = False
 
-    for i in nums:
-        if sum(a[0:i + 1]) % 2 == 0:
-            ben += 1
+    # Precompute the number of primes up to each index
+    prime_counts = [0] * (max_n + 1)
+    count = 0
+    for i in range(2, max_n + 1):
+        if sieve[i]:
+            count += 1
+        prime_counts[i] = count
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n < 2:
+            ben_wins += 1
+            continue
+        primes_up_to_n = prime_counts[n]
+        if primes_up_to_n % 2 == 1:
+            maria_wins += 1
         else:
-            maria += 1
-    if ben > maria:
-        return "Ben"
-    if maria > ben:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    return None
-
-
-def rm_multiples(ls, x):
-    for i in range(2, len(ls)):
-        try:
-            ls[i * x] = 0
-        except (ValueError, IndexError):
-            break
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
--- 0x0A-primegame/README.md
+++ 0x0A-primegame/README.md
@@ -4,3 +4,4 @@ create the 0 file
 update 0
 create a main file
 update 0
+update
