74d7f5d4b5fa37607580baed9a8b04d7baca7a6d
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-10-03 04:08:40 -0700

    {create the project dir}

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter.py
+
+This module provides a function to calculate
+the perimeter of an island in a grid.
+"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of the island in the given grid."""
+    if not grid or not grid[0]:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
